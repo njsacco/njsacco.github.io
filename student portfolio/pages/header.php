d06307708a63a39f7e67fe10426d211f1e828848
Noah Sacco <35784016+njsacco@users.noreply.github.com>
2023-03-10 13:05:09 -0600

    added student portfolio folder

---
+++ student portfolio/pages/header.php
@@ -0,0 +1,9 @@
+<header>
+  <h1>My Student Portfolio</h1>
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="pages/projects.php">Projects</a>
+    <a href="pages/about.php">About Me</a>
+    <a href="pages/contact.php">Contact</a>
+  </nav>
+</header>
